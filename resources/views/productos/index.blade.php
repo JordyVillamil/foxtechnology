de407df23101be130e7d0bdc99df2b2baa826700
JordyVillamil <jordyvillamilletras@gmail.com>
2024-11-12 16:26:45 -0500

    se actualiza index ya que presenta errores

---
+++ resources/views/productos/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('titulo', 'Nuestros Productos')
+
+@section('contenido')
+
+    @foreach ($productos as $producto)
+        <div class="card bg-base-100 w-96 shadow-xl">
+           <figure class="px-10 pt-10">
+              <img
+               src="https://picsum.photos/id/{{$producto->id}}/240"
+               alt="{{$producto->nombre}}"
+               class="rounded-xl" />
+          </figure>
+             <div class="card-body items-center text-center">
+              <h2 class="card-title">foxtechnology</h2>
+              <p>software a tu alcance</p>
+             <div class="card-actions">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+        
+    @endforeach
+
+@endsection
