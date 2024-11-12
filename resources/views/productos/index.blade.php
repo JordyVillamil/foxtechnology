e5d3a3c9f566675885a6803a6e6a8b0e33a2960d
JordyVillamil <jordyvillamilletras@gmail.com>
2024-11-12 16:33:24 -0500

    intento de correccion de errores en las vistas de productos

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -6,21 +6,20 @@
 
     @foreach ($productos as $producto)
         <div class="card bg-base-100 w-96 shadow-xl">
-           <figure class="px-10 pt-10">
-              <img
-               src="https://picsum.photos/id/{{$producto->id}}/240"
-               alt="{{$producto->nombre}}"
-               class="rounded-xl" />
-          </figure>
-             <div class="card-body items-center text-center">
-              <h2 class="card-title">foxtechnology</h2>
-              <p>software a tu alcance</p>
-             <div class="card-actions">
-            <button class="btn btn-primary">Buy Now</button>
-          </div>
+            <figure class="px-10 pt-10">
+                <img
+                    src="https://picsum.photos/id/{{$producto->id}}/240"
+                    alt="{{ $producto->nombre }}"
+                    class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">{{ $producto->nombre }}</h2>
+                <p>{{ $producto->descripcion }}</p>
+                <div class="card-actions">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
         </div>
-      </div>
-        
     @endforeach
 
 @endsection
