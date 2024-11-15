42f831680d950ab87733c2e58bdcd6af2d696893
JordyVillamil <jordyvillamilletras@gmail.com>
2024-11-14 19:55:40 -0500

    se crean metodos crud y los layouts edit y create de productos

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -3,9 +3,14 @@
 @section('titulo', 'Nuestros Productos')
 
 @section('contenido')
+{{-- Botón para crear un producto nuevo --}}
+<div class="flex justify-end m-4">
+    <a href="{{ route('productos.create') }}" class="btn btn-outline">Nuevo producto</a>
+    </div>
 
+    <div class= "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-6">
     @foreach ($productos as $producto)
-        <div class="card bg-base-100 w-96 shadow-xl">
+        <div class="card bg-base-100 w-96 shadow-xl m-8">
             <figure class="px-10 pt-10">
                 <img
                     src="https://picsum.photos/id/{{$producto->id}}/240"
@@ -15,6 +20,14 @@
             <div class="card-body items-center text-center">
                 <h2 class="card-title">{{ $producto->nombre }}</h2>
                 <p>{{ $producto->descripcion }}</p>
+                precio: <div class="badge badge-outline">{{ $producto-> precio}}</div>
+                Stock: <div class="badge badge-outline">{{ $producto-> stock}}</div>
+                   <a href="{{ route('productos.edit', $producto->id) }}" class="btn btn-outline btn-xs" >Editar</a>
+                   <form action="{{ route('productos.destroy', $producto->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-outline btn-xs">Eliminar</button>
+                   </form>                   
                 <div class="card-actions">
                     <button class="btn btn-primary">Buy Now</button>
                 </div>
