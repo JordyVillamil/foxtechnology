e5c165aa3776b9a5c19ae9da17077aa6ae0fc741
JordyVillamil <jordyvillamilletras@gmail.com>
2024-11-23 13:31:58 -0500

    se realizan pruebas de cambio de contraseña

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,27 +1,22 @@
 @extends('layouts.app')
-
-@section('cabecera', 'Editar perfil')
-
+@section('titulo','Dashboard')
+@section('cabecera', 'Dashboard')
 @section('contenido')
- <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-      <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-       <div class="max-w-xl">
-      
-        @include('profile.partials.update-profile-information-form')
-     </div>
-   </div>
-    <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-       <div class="max-w-xl">
-      @include('profile.partials.update-password-form')
-    </div>
-  </div>
-
-     <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-       <div class="max-w-xl">
-         @include('profile.partials.delete-user-form')
+    <div class="py-6">
+      <div class="max-w-7xl mx-auto sm:px-2 lg:px-4">
+         <div class="hero min-h-screen bg-base-200 rounded-2xl">
+          <div class="hero-content text-center">
+           <div class="max-w-md">
+            <h1 class="text-5xl font-bold">Hola {{ auth()->user()->name }}</h1>
+            <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut
+assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id
+nisi.</p>
+       <a href="{{ route('productos.index') }}" class="btn btn-primary">Consultar
+productos</a>
+          </div>
+        </div>
        </div>
-     </div>
+      </div>
    </div>
- </div>
 @endsection
+
